cc94673ddb073feace130007eab15473c5d9db5f
Swetha S <swetha.s2022cce@sece.ac.in>
2024-05-30 16:54:51 +0530

    Removed the underlines and made the syllabus appear centered

--- src/components/page3/3Year.js
+++ src/components/page3/3Year.js
@@ -8,7 +8,7 @@ import syllabus from '../../assets/icons8-syllabus-80 (1).png';
 import pyq from "../../assets/icons8-minus-1-year-80.png"
 import Notes from "../../assets/icons8-notes-80.png"
 import youtube from "../../assets/icons8-youtube-64.png";
-
+import '../../index.css'
 
 function TrdYear() {
 
@@ -38,8 +38,8 @@ function TrdYear() {
     </div>
     </Link>
 
-   <Link class="link" to='/trdsyllabus'>
-    <div class="card pink">
+   <Link class="link" to='/trdsyllabus' >
+    <div class="card pink" >
     <img src={syllabus} alt="" />
       <p class="second-text">Syllabus</p>
     </div> 
--- src/components/page8(3yr)/trdsyllabus.js
+++ src/components/page8(3yr)/trdsyllabus.js
@@ -4,6 +4,7 @@ import Footer from "../../pages/footer";
 import '../page4/syllabus.css';
 
 
+
 const trdsyllabus =() => {
 
     // const[selectSem, setSelectSem] = useState(null);
